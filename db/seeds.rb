c5ac4ccac226a65d2c70b22d0c42a5b231d06572
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-08-28 19:54:13 -0300

    added users to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 require 'faker'
 
+admin = User.new(email: 'admin@admin.com', password: '123456', admin: true)
+admin.save
+
+user = User.new(email: 'a@a.com', password: '123456')
+user.save
+
 50.times do
   post = Post.new(head_title: Faker::Lorem.sentence, subtitle: Faker::Lorem.sentence, title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
   post.save
