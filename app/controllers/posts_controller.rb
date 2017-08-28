8af39091a75d8077e3acc5a270745fbae5632bce
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-08-28 13:12:08 -0300

    Created the shared folder in view. Implemented the skip_before_action on authenticate to index and show pages of Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
+  # Skips authentication on index and show pages
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
 
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
   end
 end
