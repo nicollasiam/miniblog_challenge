2942550b64a4c355ae5812b3c6937af6e7629889
Nicollas Isabel <nicollasisabel@hotmail.com>
2017-08-28 15:16:26 -0300

    Added constrains to user and post models. Also added has_many and belongs_to keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :validatable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
+
+  validates :email, uniqueness: true, presence: true
 end
